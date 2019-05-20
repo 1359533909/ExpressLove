fbcb7d2279ac77031984ac57d2f329b4b3d5da2c
mxl <1359533909@qq.com>
2019-05-20 19:15:16 +0800

    love you

---
+++ src/main/java/com/example/demo/MxlLoveLdlApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MxlLoveLdlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MxlLoveLdlApplication.class, args);
+	}
+
+}
